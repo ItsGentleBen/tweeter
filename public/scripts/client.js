891be5cbccc5cd47e08e1375c260e50692caba49
Ben Stolb <benstolb@protonmail.com>
2022-11-09 15:22:31 -0800

    Add a function to create tweet element from tweet obj

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,48 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+createTweetElement = function (tweet) {
+
+  const $tweet = $(`
+    <article class="tweet">
+    <header>
+      <div>
+        <img src="${tweet.user.avatars}">
+        <span>${tweetData.user.name}</span>
+      </div>
+      <span class="handle">${tweet.user.handle}</span>
+    </header>
+    <p class="contents">${tweet.content.text}</p>
+    <footer>
+      <p>${tweet.created_at}</p>
+      <div class="action-icons">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+    </div>
+    </footer>
+  </article>`);
+
+  return ($tweet);
+};
+
+
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
